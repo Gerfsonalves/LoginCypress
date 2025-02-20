d4128ec4e76aac997240501a4cd4a8bf26590a2c
Gerfson Alves <gerfson17@hotmail.com>
2025-02-20 17:27:17 -0300

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://jamesonbatista.github.io/projectqatesterweb/login.html')
+  })
+})
