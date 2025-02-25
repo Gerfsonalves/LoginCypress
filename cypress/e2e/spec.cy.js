d10bd516a9dc050b0a9f38967b29645a3dc437b5
Gerfson Alves <gerfson17@hotmail.com>
2025-02-24 21:52:30 -0300

    Cadastro

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,6 +7,9 @@ describe('template spec', () => {
     cy.get('#username').type('JoseSilva@email.com')
     cy.get('#password').type('12345678')
     cy.get('#loginForm > button').click()
+
+    cy.url().should('include', 'https://jamesonbatista.github.io/projectqatesterweb/cadastro-simples.html')
+    cy.title().should('eq', 'Cadastro Simples')
   });
 
   it('Cadastro', () => {
@@ -15,6 +18,16 @@ describe('template spec', () => {
     cy.get('#password').type('12345678')
     cy.get('#gender').select('Masculino')
     cy.get('#birthdate').type('12/12/1912')
+    cy.get('#phone').type('551190000000')
+    cy.get('#address').type('Avenida 244')
+    cy.get('#state').select('São Paulo')
+    cy.get('#city').should('have.value', 'São Paulo')
+    cy.get('#terms').click()
+    cy.get('#cadastroForm > button').click()
+
+    cy.get('#alertMessage')
+      .should('be.visible')
+      .and('have.text', 'Cadastro realizado com sucesso!')
   });
 
 })
