721c867a1e2731db398a266a2595899683b3f14e
Gerfson Alves <gerfson17@hotmail.com>
2025-02-24 21:36:16 -0300

    Cadastro

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,20 @@
 /// <reference types="cypress" />
 
 describe('template spec', () => {
-  it('passes', () => {
+  beforeEach('Acesso a página', () => {
     cy.visit('https://jamesonbatista.github.io/projectqatesterweb/login.html')
-  })
+
+    cy.get('#username').type('JoseSilva@email.com')
+    cy.get('#password').type('12345678')
+    cy.get('#loginForm > button').click()
+  });
+
+  it('Cadastro', () => {
+    cy.get('#name').type('José silva')
+    cy.get('#email').type('JoseSilva@email.com')
+    cy.get('#password').type('12345678')
+    cy.get('#gender').select('Masculino')
+    cy.get('#birthdate').type('12/12/1912')
+  });
+
 })
